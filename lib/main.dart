d79b637a6f92a9d3b2ce0c58718dd5ce9e6074a1
k1 <keyvanramezan@gmail.com>
2020-08-19 22:25:07 +0430

    center snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,22 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(MyApp());
 }
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  TextEditingController user,pass;
+  TextEditingController user, pass;
+  String passError, userError;
+  SnackBar alarm;
+  GlobalKey<ScaffoldState> scaffoldKey;
   @override
   void initState() {
     user = TextEditingController();
     pass = TextEditingController();
+    scaffoldKey = GlobalKey<ScaffoldState>();
     super.initState();
   }
 
@@ -23,6 +28,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          key: scaffoldKey,
           appBar: AppBar(
             centerTitle: true,
             title: Text("ورود به سیستم"),
@@ -43,33 +49,36 @@ class _MyAppState extends State<MyApp> {
                   textAlign: TextAlign.center,
                   style: TextStyle(color: Colors.white),
                   decoration: InputDecoration(
+                      errorText: userError,
                       fillColor: Colors.white12,
                       filled: true,
                       contentPadding:
-                      EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                          EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
                       hintText: "نام کاربری",
                       hintStyle: TextStyle(color: Colors.white54),
                       border: OutlineInputBorder(
                           borderRadius:
-                          BorderRadius.all(Radius.circular(8.0)))),
+                              BorderRadius.all(Radius.circular(8.0)))),
                 ),
               ),
               Container(
                 margin: EdgeInsets.fromLTRB(35.0, 10.0, 35.0, 10.0),
                 child: TextField(
                   controller: pass,
+                  obscureText: true,
                   textAlign: TextAlign.center,
                   style: TextStyle(color: Colors.white),
                   decoration: InputDecoration(
+                      errorText: passError,
                       fillColor: Colors.white12,
                       filled: true,
                       contentPadding:
-                      EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                          EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
                       hintText: "کلمه عبور",
                       hintStyle: TextStyle(color: Colors.white54),
                       border: OutlineInputBorder(
                           borderRadius:
-                          BorderRadius.all(Radius.circular(8.0)))),
+                              BorderRadius.all(Radius.circular(8.0)))),
                 ),
               ),
               Container(
@@ -80,8 +89,25 @@ class _MyAppState extends State<MyApp> {
                     color: Colors.blueAccent,
                     padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
                     onPressed: () {
-                        FocusScope.of(context).requestFocus(FocusNode());
-                        print("user ${user.text}, password: ${pass.text}");
+                      FocusScope.of(context).requestFocus(FocusNode());
+                      setState(() {
+                        if (user.text.length < 4) {
+                          userError =
+                              "Please enter at least 4 letters at userName ";
+                        } else if (pass.text.length < 4) {
+                          passError =
+                              "Please enter at least 4 letters at password ";
+                          userError = null;
+                        } else {
+                          userError = null;
+                          passError = null;
+                          alarm = SnackBar(
+                            content:
+                                Text("Login Successful Welcome ${user.text}", textAlign: TextAlign.center,),
+                          );
+                          scaffoldKey.currentState.showSnackBar(alarm);
+                        }
+                      });
                     },
                     textColor: Colors.white,
                     child: Text("ورود"),
@@ -92,5 +118,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-
